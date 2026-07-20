5cda6b702091be6b03a9565e26bbfb2a86b46965
tou-eng <toufik.menaa01@gmail.com>
2026-07-20 18:02:28 +0100

    1st lession list

---
+++ linked_lists/lession.py
@@ -0,0 +1,35 @@
+#List = [] ordered and unordered collection of data duplicates allowed
+#Tuple = () ordered and unordered collection of data duplicates allowed
+#Set = {} ordered and unordered collection of data duplicates not allowed
+
+#list
+# fruits = ["banana", "apple", "cherry","mango", "kiwi"]
+#fruits.append("orange") #add at the end
+#fruits.remove("banana") #remove banana
+# fruits.insert(3, "watermelon") #insert at index 3
+#fruits.pop(1) #remove at index 1
+#fruits.sort() #sort the list
+#fruits.reverse() #reverse the list
+#fruits.clear() #clear the list
+#fruits.extend(["grape", "pear"]) #add multiple items
+# print(fruits)
+
+#set
+fruits = {"banana", "apple", "cherry","mango", "kiwi"}
+
+
+# 2dlist = [list1 , list2....]
+# fruits = ["apple", "banana", "cherry"]
+# vegetables = ["carrot", "broccoli", "spinach"]
+# meats = ["chicken", "beef", "pork"]
+# food = [fruits, vegetables, meats]
+# for category in food:
+#     for item in category:
+#         print(item , end = " ")
+#     print()
+
+# number = ((1, 2, 3), (4, 5, 6), (7, 8, 9), ("*", "0", "*"))
+# for row in number:
+#     for col in row:
+#         print(col, end=" ")
+#     print()
